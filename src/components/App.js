e59515879a26dff6c39b998553e427a76a21cea2
Alexander Repeta <alexander.repeta@gmail.com>
2018-11-16 21:14:22 +0200

    Added lesson 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import Panel from './Panel';
-import ProfileDetails from './ProfileDetails';
-import TechList from './TechList';
-import ProductList from './ProductList';
-import products from '../products.json';
+import React, { Component } from 'react';
+import Counter from './Counter';
+import StepChanger from './StepChanger';
 
-const tech = [
-  { id: 'id-1', name: 'JS' },
-  { id: 'id-2', name: 'React' },
-  { id: 'id-3', name: 'React Router' },
-  { id: 'id-4', name: 'Redux' }
-];
+export default class App extends Component {
+  state = {
+    counterStep: 0
+  };
 
-const App = () => (
-  <div>
-    <Panel title="Latest tech">
-      <TechList items={tech} />
-    </Panel>
+  handleChangeStep = () => {
+    this.setState(prevState => ({
+      counterStep: prevState.counterStep + 1
+    }));
+  };
 
-    <Panel title="Top seeling products">
-      <ProductList products={products} />
-    </Panel>
-  </div>
-);
+  render() {
+    const { counterStep } = this.state;
 
-export default App;
+    return (
+      <div>
+        <StepChanger
+          currentStep={counterStep}
+          onUpdateStep={this.handleChangeStep}
+        />
+        <Counter step={counterStep} initialValue={10} />
+      </div>
+    );
+  }
+}
