842e0a6d285fff2a2f866861c9a43d80ef44b93a
Alexander Repeta <alexander.repeta@gmail.com>
2018-12-21 21:20:33 +0200

    Added counter example

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Title from './Title';
-import Nav from './Nav';
-import HomePage from '../pages/HomePage';
-import ArticlesPage from '../pages/ArticlesPage';
-import ArticlePage from '../pages/ArticlePage';
-import AboutPage from '../pages/AboutPage';
-import NotFound from '../pages/NotFoundPage';
+import Counter from './Counter';
+import StepSelector from './StepSelector';
 
 const App = () => (
   <>
-    <Title text="React Router Basics" />
-    <Nav />
-
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/articles" component={ArticlesPage} />
-      <Route path="/articles/:id" component={ArticlePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route component={NotFound} />
-    </Switch>
+    <StepSelector />
+    <Counter />
   </>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
 import App from './components/App';
+
 import './index.module.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Route component={App} />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
