0791f0877bc2c8160964cf7c7523ce3424181962
Alexander Repeta <alexander.repeta@gmail.com>
2019-01-18 20:49:16 +0200

    Adds example

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/App/App';
-import store from './redux/store';
 import './index.css';
 
-render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <Route component={App} />
-        </Provider>
-    </BrowserRouter>,
-    document.getElementById('root'),
-);
+render(<App />, document.getElementById('root'));
